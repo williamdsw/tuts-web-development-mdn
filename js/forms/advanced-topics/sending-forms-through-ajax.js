eca0f208d70e6748a9e8a88f6d18c259c4f09209
williamdsw <williamdsw@outlook.com>
2020-08-19 17:06:52 -0300

    Sending forms through JavaScript

---
+++ js/forms/advanced-topics/sending-forms-through-ajax.js
@@ -0,0 +1,36 @@
+
+window.addEventListener('load', function (){
+    const buttonRawData = document.querySelector('#buttonRawData');
+    const buttonFormData = document.querySelector('#buttonFormData');
+    const myForm = document.querySelector('#myForm');
+
+    buttonRawData.addEventListener('click', () => sendRawData ({ test: 'ok!' }));
+    buttonFormData.addEventListener('click', () => sendFormData (myForm));
+});
+
+function sendRawData(data) {
+    const request = new XMLHttpRequest();
+
+    let urlEncodedDataPairs = [];
+    for (let name in data) {
+        urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+    }
+
+    const urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    request.addEventListener('load', () => alert('Data sent!'));
+    request.addEventListener('error', () => alert('Something went wrong!'));
+    request.open('POST', 'https://example.com/cors.php');
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(urlEncodedData);
+}
+
+function sendFormData(form) {
+    const request = new XMLHttpRequest();
+    const formData = new FormData(form);
+
+    request.addEventListener('load', (ev) => alert(ev.target.responseText));
+    request.addEventListener('error', () => alert('Something went wrong!'));
+    request.open('POST', 'https://example.com/cors.php');
+    request.send(formData);
+}
