d52ddb7e6814ab97ec858e68e2db80d2aeb59520
williamdsw <williamdsw@outlook.com>
2020-08-20 19:52:08 -0300

    Storing the information you need - Variables

---
+++ js/learn-js/first-steps/variables.js
@@ -0,0 +1,31 @@
+// FIELDS
+
+let myAge = 25;
+let myHeight = 1.75;
+let myName = 'William';
+let iAmAlive = true;
+let favoriteBands = ['Megadeth', 'Metallica', 'Iron Maiden', 'Queen'];
+
+let myself = {
+    age: 25, height: 1.75, name: 'William', iAmAlive: true, 
+    favoriteBands: ['Megadeth', 'Metallica', 'Iron Maiden', 'Queen']
+};
+
+const daysInWeek = 7;
+
+// FUNCTIONS
+
+window.addEventListener('load', function () {
+    const textareaInformation = document.getElementById('textareaInformation');
+    textareaInformation.innerHTML += `Days in Week: ${daysInWeek}\n\n`;
+    textareaInformation.innerHTML += `Age: ${myAge}\n`;
+    textareaInformation.innerHTML += `Height: ${myHeight}\n`;
+    textareaInformation.innerHTML += `Name: ${myName}\n`;
+    textareaInformation.innerHTML += `Am I Alive: ${iAmAlive}\n`;
+    textareaInformation.innerHTML += `Favorite bands: ${favoriteBands}\n\n`;
+    
+    for (const prop of Object.keys(myself)) {
+        textareaInformation.innerHTML += `${prop} : ${myself[prop]}\n`;
+    }
+});
+
