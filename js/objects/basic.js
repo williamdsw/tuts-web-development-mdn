49b902c54a8d1b877d8bbb9fb31d1eed63386d09
williamdsw <williamdsw@outlook.com>
2020-08-25 14:01:15 -0300

    JavaScript object basics

---
+++ js/objects/basic.js
@@ -0,0 +1,33 @@
+
+const person = {
+    name: ['Robert', 'Smith'],
+    age: 61,
+    gender: 'male',
+    interests: ['Batman', 'Goth', 'Forest', 'Make-up'],
+    other: {
+        height: 1.75,
+        weight: 80,
+        occupations: ['Singer', 'Guitarrist', 'Composer']
+    },
+
+    bio: function () {
+        return `${this.name.join(' ')} is ${this.age} years old! He likes ${this.interests.join(', ')}.`;
+    },
+
+    otherInfo: function() {
+        return `\nHeight: ${this.other.height} cm.\n Weight: ${this.other.weight} kg.\n Occupations: ${this.other.occupations.join(', ')}.`;
+    },
+
+    greeting: function() { alert(`Hi! I'm ${this.name[0]}`) }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    const info = document.getElementById('info');
+    const buttonGreeting = document.querySelector('button');
+
+    console.log ('person', person);
+
+    info.textContent = person.bio();
+    info.textContent += person.otherInfo();
+    buttonGreeting.addEventListener('click', () => { person.greeting (); });
+});
